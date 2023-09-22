bb31f0e870a831d313b08ce581f4f0d438acc53f
Jessica Neves <118688096+jessic4neves@users.noreply.github.com>
2023-09-22 16:54:55 -0300

    Nivelamento em Python

---
+++ Nivelamento-frontend.py
@@ -0,0 +1,30 @@
+
+maior_altura = float("-inf")
+menor_altura = float("inf")
+soma_altura_homens = 0
+quantidade_mulheres = 0
+
+
+for contador in range(16):
+    altura = float(input("Digite a sua altura: "))
+    genero = input("Digite o seu gênero ").upper()
+
+    if altura > maior_altura:
+        maior_altura = altura
+    if altura < menor_altura:
+        menor_altura = altura
+
+    if genero == "M":
+        soma_altura_homens += altura
+    elif genero == "F":
+        quantidade_mulheres += 1
+
+
+media_altura_homens = soma_altura_homens / (15 - quantidade_mulheres)
+
+
+print("Maior altura:", maior_altura)
+print("Menor altura:", menor_altura)
+print("Média da altura dos homens:", media_altura_homens)
+print("Quantidade de mulheres:", quantidade_mulheres)
+
